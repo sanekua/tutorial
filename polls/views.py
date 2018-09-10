197b5a15d013c79f77c95a6fa1bc83a49c36d6bd
sanekua <you@example.com>
2018-09-11 00:38:27 +0300

    after 04 tutorial

--- polls/views.py
+++ polls/views.py
@@ -2,24 +2,33 @@ from django.shortcuts import render
 
 
 from django.http import HttpResponse, HttpResponseRedirect
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
-# ...
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -39,16 +48,6 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
-from django.http import HttpResponse
-from django.template import loader
-
-
-from .models import Question
-
-
-
-
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -56,45 +55,6 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
-
-
-from django.shortcuts import render
-from django.views import View
-
-
-def my_index_view(request):
-    data = {'text':"Hello world"}
-    render(request,'university/custom_html.html',data)
-
-
-class MyView(View):
-    data = {'text': "Hello world"}
-    #form = StudentForm
-    template_name = 'university/custom_html.html'
-
-    def get(self, request, *args, **kwargs):
-        #form = self.form_class(initial=self.initial)
-        return render(request, self.template_name,self.data)
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            # <process form cleaned data>
-            return HttpResponseRedirect('/success/')
-
-        return render(request, self.template_name, {'form': form})
-
-
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, "index.html")
-
-
-
-
-from django.shortcuts import get_object_or_404, render
 
 
 def results(request, question_id):
