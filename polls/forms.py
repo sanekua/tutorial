1a71c1a598ef7768e61b6764ca6c3f0811820522
sanekua <you@example.com>
2018-09-11 01:07:12 +0300

    after 05 tutorial? making tests

--- polls/forms.py
+++ polls/forms.py
@@ -1,9 +1,4 @@
 from django import forms
 from .models import Student
 
-class CustomerForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ('question_text', 'pub_date')
-
 
