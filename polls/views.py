5a951a297d0596986b5cbcda73c65e0b288af585
sanekua <you@example.com>
2018-09-11 00:26:44 +0300

    4 step

--- polls/views.py
+++ polls/views.py
@@ -1,67 +1,88 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+
+from django.http import HttpResponse, HttpResponseRedirect
 
 
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
-#
-# def detail(request, question_id):
-#     return HttpResponse("You're looking at question %s." % question_id)
-#
-# def results(request, question_id):
-#     response = "You're looking at the results of question %s."
-#     return HttpResponse(response % question_id)
-#
-# def vote(request, question_id):
-#     return HttpResponse("You're voting on question %s." % question_id)
-#
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
+# ...
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 from django.http import HttpResponse
 from django.template import loader
 
-#
-# from .models import Question
-#
-#
-#
-#
-#
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
 
-#
-# from django.shortcuts import render
-# from django.views import View
-#
-#
-# def my_index_view(request):
-#     data = {'text':"Hello world"}
-#     render(request,'university/custom_html.html',data)
-#
-#
-# class MyView(View):
-#     data = {'text': "Hello world"}
-#     #form = StudentForm
-#     template_name = 'university/custom_html.html'
-#
-#     def get(self, request, *args, **kwargs):
-#         #form = self.form_class(initial=self.initial)
-#         return render(request, self.template_name,self.data)
+from .models import Question
 
-    # def post(self, request, *args, **kwargs):
-    #     form = self.form_class(request.POST)
-    #     if form.is_valid():
-    #         # <process form cleaned data>
-    #         return HttpResponseRedirect('/success/')
-    #
-    #     return render(request, self.template_name, {'form': form})
+
+
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+from django.shortcuts import render
+from django.views import View
+
+
+def my_index_view(request):
+    data = {'text':"Hello world"}
+    render(request,'university/custom_html.html',data)
+
+
+class MyView(View):
+    data = {'text': "Hello world"}
+    #form = StudentForm
+    template_name = 'university/custom_html.html'
+
+    def get(self, request, *args, **kwargs):
+        #form = self.form_class(initial=self.initial)
+        return render(request, self.template_name,self.data)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # <process form cleaned data>
+            return HttpResponseRedirect('/success/')
+
+        return render(request, self.template_name, {'form': form})
 
 
 from django.shortcuts import render
@@ -69,3 +90,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+
+
+from django.shortcuts import get_object_or_404, render
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
